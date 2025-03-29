64c094dc1c8e6cd4007aa6a4669babbe476c3559
Kaleb Minassie <126221144+kaleb-minassie@users.noreply.github.com>
2025-03-28 22:40:50 -0400

    updated mychip8.h

--- mychip8.h
+++ mychip8.h
@@ -1,1 +1,18 @@
+#ifndef MYCHIP8_H
+#define MYCHIP8_H
 
+/* These functions are available to you to use in your code. */
+uint8_t chip8_mem_read(uint16_t addr);
+void chip8_mem_write(uint16_t addr, uint8_t val);
+uint8_t chip8_register_read(uint8_t reg);
+void chip8_register_write(uint8_t reg, uint8_t val);
+void chip8_clear_frame(void);
+void chip8_mem_clear(void);
+int chip8_draw_sprite(uint16_t addr, uint8_t x, uint8_t y, uint8_t height);
+void chip8_mem_reset(void);
+
+/* Key statuses are defined as registers 0x00-0x0F */
+#define CHIP8_REG_DT        0x10
+#define CHIP8_REG_ST        0x11
+
+#endif // MYCHIP8_H
